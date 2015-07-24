1f84d6161c029d125c7f8d9a8b6737209238c17a
madao <fairright@sina.com>
2015-07-24 13:07:09 +0800

    初始化项目

---
+++ Beggar/Beggar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Beggar
+//
+//  Created by Madao on 15/7/24.
+//  Copyright (c) 2015年 GanZhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
